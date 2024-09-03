305416b7c2a149aa5bffd2f8d79268663ebbd976
muendokennedy <kennedymunyao999@gmai.com>
2024-09-03 15:43:12 +0300

    Made the entire median function accessible in one entry module

--- lib/elixirstuts.ex
+++ lib/elixirstuts.ex
@@ -1,6 +1,7 @@
 defmodule Elixirstuts do
-  alias Elixirstuts.CentralTendency.Mean
+  alias Elixirstuts.CentralTendency.{Mean, Median}
   # def population_mean(nums), do: Mean.population_mean(nums)
   defdelegate population_mean(nums), to: Mean
   defdelegate sample_mean(nums), to: Mean
+  defdelegate median(num_list), to: Median
 end
