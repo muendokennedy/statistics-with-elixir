cf3305f3094cb3d687cbd12a4ef0de0ebd922ef7
muendokennedy <kennedymunyao999@gmai.com>
2024-09-03 15:33:19 +0300

    Implemented a functionality to calculate the median of a list of numbers

---
+++ lib/central_tendency/median.ex
@@ -0,0 +1,28 @@
+defmodule Elixirstuts.CentralTendency.Median do
+
+  require Integer
+  alias Elixirstuts.{Errors, Validators}
+
+  # =====================================================================
+
+  def median(num_list) when is_list(num_list) do
+    num_list |> Validators.validate_num_list() |> calc_median()
+  end
+
+  def median(_), do: Errors.invalid_data_type()
+  # =====================================================================
+  defp calc_median({:error, _msg}), do: Errors.invalid_data_type()
+  defp calc_median({false, _}), do: Errors.invalid_data_type()
+  defp calc_median({true, num_list}) do
+    count = num_list |> Enum.count()
+    num_list |> Enum.sort() |> get_median(Integer.is_even(count), count)
+  end
+
+  # =====================================================================
+  defp get_median(num_list, false, count), do: Enum.at(num_list, div(count, 2))
+  defp get_median(num_list, true, count) do
+    a = Enum.at(num_list, div(count - 1, 2))
+    b = Enum.at(num_list, div(count, 2))
+    (a + b) / 2
+  end
+end
