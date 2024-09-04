a34fc5312ac262e3570e5097f4e706362e634226
muendokennedy <kennedymunyao999@gmai.com>
2024-09-04 16:16:45 +0300

    Implemented the functionality to calculate mode using elixir funcions

---
+++ lib/central_tendency/mode.ex
@@ -0,0 +1,16 @@
+defmodule Elixirstuts.CentralTendency.Mode do
+
+# =======================================================================
+
+  def mode(nums) when is_list(nums) do
+    ranked_map = nums |> Enum.frequencies()
+
+    ranked_map |> Map.values() |> Enum.max() |> mode_func(ranked_map)
+  end
+
+  defp mode_func(1, _ranked_map), do: nil
+  defp mode_func(max, ranked_map) do
+    ranked_map |> Map.filter(fn {_key, value} -> value == max end)
+    |> Map.keys()
+  end
+end
--- lib/elixirstuts.ex
+++ lib/elixirstuts.ex
@@ -1,7 +1,8 @@
 defmodule Elixirstuts do
-  alias Elixirstuts.CentralTendency.{Mean, Median}
+  alias Elixirstuts.CentralTendency.{Mean, Median, Mode}
   # def population_mean(nums), do: Mean.population_mean(nums)
   defdelegate population_mean(nums), to: Mean
   defdelegate sample_mean(nums), to: Mean
   defdelegate median(num_list), to: Median
+  defdelegate mode(num_list), to: Mode
 end
